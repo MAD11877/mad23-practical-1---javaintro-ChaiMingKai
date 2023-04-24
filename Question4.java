e4c552f02da87b83cc74d4262cc508e319328acc
ChaiMingKai <116235991+ChaiMingKai@users.noreply.github.com>
2023-04-24 14:48:15 +0800

    2

--- Question4.java
+++ Question4.java
@@ -25,7 +25,7 @@ public class Question4
       for (int j = 1; j <= i; j++) {
         System.out.print("*");
       }
-      System.out.println();
+      System.out.println();}
     
         
       

7bd88a430648c195c051e17a739aeed5aa3f7038
ChaiMingKai <116235991+ChaiMingKai@users.noreply.github.com>
2023-04-24 21:46:11 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,12 +11,12 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter user's height(m) :");
-    double height = in.nextDouble();
-    
-    System.out.print("Enter user's weight(kg) :");
-    double weight = in.nextDouble();
-    
+    /**System.out.print("Enter user's height(m) :");
+    *double height = in.nextDouble();
+    *
+    *System.out.print("Enter user's weight(kg) :");
+    *double weight = in.nextDouble();
+    */
     double bmi= weight/(height*height);
     
     System.out.printf("Bmi is %.1f",bmi);

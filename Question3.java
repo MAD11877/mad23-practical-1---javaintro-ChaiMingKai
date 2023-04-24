18df2a716399cda4029947d10c516fe44045b383
ChaiMingKai <116235991+ChaiMingKai@users.noreply.github.com>
2023-04-24 21:53:06 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,7 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter the integer: ");
+
     int integer=in.nextInt();
     int square=integer*integer;
     
